1b47dcc1c9cd70a0ef57aaf1611b992707e9ca88
HAOCI_ZHANG <zhanghaoci@gmail.com>
2017-11-02 19:19:55 -0400

    added sched_wrr_entity

--- kernel/include/linux/sched.h
+++ kernel/include/linux/sched.h
@@ -1023,6 +1023,13 @@ struct sched_rt_entity {
 };
 
 
+struct sched_wrr_entity {
+	unsigned int time_slice;
+	struct list_head list;
+	struct wrr_rq *wrr_rq;
+	struct wrr_rq *my_q;
+};
+
 struct rcu_node;
 
 enum perf_event_task_context {

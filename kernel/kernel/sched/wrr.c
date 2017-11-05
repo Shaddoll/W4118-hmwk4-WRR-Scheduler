3307aa7d0fb71c945c2aecb2cc22766684dec1a0
HAOCI_ZHANG <zhanghaoci@gmail.com>
2017-11-05 04:32:35 -0500

    fixed bugs

--- kernel/kernel/sched/wrr.c
+++ kernel/kernel/sched/wrr.c
@@ -7,7 +7,7 @@
  *  handled in sched/fair.c)
  */
  
-#define WRR_TIMESLICE		(100 * HZ / 1000)
+#define WRR_TIMESLICE		(10 * HZ / 1000)
 
 #ifdef CONFIG_SMP
 static int
@@ -138,8 +138,10 @@ static void task_tick_wrr(struct rq *rq, struct task_struct *p, int queued)
 		return;
 
 	p->wre.time_slice = WRR_TIMESLICE;
-	if (p->wre.weight > 1)
-		p->wre.weight = p->wre.weight - 1;	
+	if (p->wre.weight > 1) { 
+		p->wre.weight = p->wre.weight - 1;
+		p->wre.time_slice = WRR_TIMESLICE * p->wre.weight;
+	}	
 	else if (wrr_se->list.prev != wrr_se->list.next) {
 		requeue_task_wrr(rq, p, 0);
 		set_tsk_need_resched(p);

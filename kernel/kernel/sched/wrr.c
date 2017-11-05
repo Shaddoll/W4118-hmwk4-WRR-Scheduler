38b36aeccb75560c1eeb6c75e2511b055e68d8a3
HAOCI_ZHANG <zhanghaoci@gmail.com>
2017-11-05 15:07:08 -0500

    debug

--- kernel/kernel/sched/wrr.c
+++ kernel/kernel/sched/wrr.c
@@ -266,3 +266,14 @@ const struct sched_class wrr_sched_class = {
 	.prio_changed		= prio_changed_wrr,
 	.switched_to		= switched_to_wrr,
 };
+
+
+void print_wrr_stats(struct seq_file *m, int cpu)
+{
+	struct wrr_rq *wrr_rq;
+
+	rcu_read_lock();
+	wrr_rq = cpu_rq(cpu)->wrr;
+	print_wrr_rq(m, cpu, wrr_rq);
+	rcu_read_unlock();
+}

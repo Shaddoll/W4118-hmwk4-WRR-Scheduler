4daf902d2eb340911f77af74e88f253ce2132904
Xianda Gao <xg2280@columbia.edu>
2017-11-07 16:51:25 -0500

    merge

---
+++ kernel/kernel/sched/test.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sched.h>
+#include <sys/types.h>
+#include <sys/syscall.h>
+#include "../../kernel/kernel/sched/wrr.h"
+
+void print(struct wrr_info *wrr_info)
+{
+	int i;
+	for (i = 0; i < wrr_info->num_cpus; i++) {
+		printf("\tcpu[%d]:\n", i);
+		printf("\t\tnr_running:\t%d\n", wrr_info->nr_running[i]);
+		printf("\t\ttotal_weight:\t%d\n", wrr_info->total_weight[i]);
+		printf("-------------\n");
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	struct wrr_info u_wrr_info;
+	int i = 0;
+	
+	syscall(245, 30);
+
+	while (1) {
+		printf("===================== %d\n", i++);
+		syscall(244, &u_wrr_info);
+		print(&u_wrr_info);
+		sleep(1);
+	}
+	
+	return 0;
+}
